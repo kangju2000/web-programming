2c8be1647995dd997f8fbf1f3aea7488f742d397
kangju2000 <kangju2000@naver.com>
2022-05-18 14:32:04 +0900

    체크 박스 생성

--- main.js
+++ main.js
@@ -7,32 +7,74 @@ let toDoList = [];
 // 로컬스토리지에 저장하는 함수
 function saveList(toDo) {
   const cnt = parseInt(localStorage.getItem('count'));
-  const data = {
+  const list = {
     toDo: toDo,
+    checked: false,
     id: cnt + 1,
   }
 
-  toDoList.push(data);
+  toDoList.push(list);
   localStorage.setItem('toDoList', JSON.stringify(toDoList));
   localStorage.setItem('count', cnt + 1);
 
-  showList(toDo, data.id);
+  showList(list);
 }
 
 // 화면에 리스트 띄우는 함수
-function showList(toDo, id) {
+function showList(list) {
   const div = document.createElement('div');
+  const input = document.createElement('input');
+  const label = document.createElement('label');
+  const modifyBtn = document.createElement('button');
+  const deleteBtn = document.createElement('button');
   div.setAttribute('class', 'list-div');
-  div.setAttribute('id', id);  // id를 심어줘서 나중에 삭제 시 찾을수 있게 하고 싶음
-  // const input = document.createElement('input');
-  // input.setAttribute('type', 'checkbox');
-  // input.setAttribute('class', 'btn-chk');
-  const p = document.createElement('p');
-  p.innerHTML = toDo;
-  // const td2 = document.createElement('td');
-  // td2.appendChild(input);
-  div.appendChild(p);
-  // div.appendChild(td2);
+  div.setAttribute('id', list.id);
+
+  input.setAttribute('type', 'checkbox');
+  input.setAttribute('class', 'chk-btn');
+  input.setAttribute('id', 'chk' + list.id);
+  input.checked = list.checked;
+  input.onclick = function (e) {
+    list.checked = e.target.checked;
+    localStorage.setItem('toDoList', JSON.stringify(toDoList));
+  };
+
+  deleteBtn.style.display = "none";
+  modifyBtn.style.display = "none";
+
+  modifyBtn.setAttribute('class', 'mod-btn');
+  modifyBtn.onclick = function (e) { };
+  modifyBtn.innerHTML = '수정';
+
+  deleteBtn.setAttribute('class', 'del-btn');
+  deleteBtn.onclick = function deleteList(e) {
+    if (!confirm('삭제하시겠습니까?')) return;
+
+    const t_list = e.target.parentNode;
+    const id = t_list.getAttribute('id');
+    for (let i = 0; i < toDoList.length; i++) {
+      if (toDoList[i].id == id) toDoList.splice(i, 1);
+    }
+    localStorage.setItem('toDoList', JSON.stringify(toDoList));
+    listBody.removeChild(t_list);
+  }
+
+  deleteBtn.innerHTML = '삭제';
+  label.innerHTML = list.toDo;
+  label.setAttribute('for', 'chk' + list.id);
+  div.onmouseover = function () {
+    deleteBtn.style.display = "block";
+    modifyBtn.style.display = "block";
+  }
+  div.onmouseout = function () {
+    deleteBtn.style.display = "none";
+    modifyBtn.style.display = "none";
+  }
+
+  div.appendChild(input);
+  div.appendChild(label);
+  div.appendChild(modifyBtn);
+  div.appendChild(deleteBtn);
   listBody.appendChild(div);
 }
 
@@ -45,23 +87,10 @@ function createList(e) {
     alert("할 일을 입력해주세요!");
   }
   else {
+
     saveList(toDo);
     listInput.value = "";
   }
-}
-
-function deleteList(e) {
-  const t_list = e.target.parentNode;
-  const id = t_list.getAttribute('id');
-  for (let i = 0; i < toDoList.length; i++) {
-    if (toDoList[i].id == id) {
-      toDoList.splice(i, 1);
-    }
-  }
-  console.log(toDoList);
-
-  localStorage.setItem('toDoList', JSON.stringify(toDoList));
-  listBody.removeChild(t_list);
 }
 
 // 리스트를 로컬스토리지에서 받아오는 함수
@@ -73,7 +102,7 @@ function getList() {
     for (let i = 0; i < lists.length; i++) {
       const list = lists[i];
       toDoList.push(list);
-      showList(list.toDo, list.id);
+      showList(list);
     };
   }
   else {
@@ -84,7 +113,6 @@ function getList() {
 function init() {
   getList();
   listForm.addEventListener("submit", createList);
-  listBody.addEventListener("click", deleteList);
 }
 
 init();
